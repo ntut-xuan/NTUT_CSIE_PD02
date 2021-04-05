903e33fd2de0eb5a6ddf092958638ca79d13505d
Xuan <69747731+ntut-xuan@users.noreply.github.com>
2021-04-05 21:04:13 +0800

    Update 15.c

--- 15.c
+++ 15.c
@@ -6,214 +6,224 @@ int max(int a, int b){
 	return a > b ? a : b;
 }
 
-char* sabs(char* a){
-	char* fx = calloc(200, sizeof(char));
-	if(a[0] == '-'){
-		for(int i = 0 ; i < strlen(a); i++){
-			fx[i] = a[i+1];
-		}
-	}else{
-		return a;
-	}
-	return fx;
-}
-
-int scmp(char* a, char* b){
+int cmp(char* a, char* b){
 	if(strlen(a) > strlen(b)){
-		return 1;
-	}else if(strlen(b) > strlen(a)){
 		return -1;
+	}else if(strlen(a) < strlen(b)){
+		return 1;
 	}else{
 		for(int i = 0; i < strlen(a); i++){
-			if(a[i] > b[i]) return 1;
-			else if(b[i] > a[i]) return -1;
+			if(a[i] > b[i]) return -1;
+			else if(a[i] < b[i]) return 1;
 		}
+		return 0;
 	}
-	return 0;
 }
 
-char* smax(char* a, char* b){
-	if(scmp(a, b) == 1){
-		return a;
+char* addn(char* a){
+	char* fix = calloc(100, sizeof(char));
+	fix[0] = '-';
+	for(int i = 1; i < strlen(a)+1; i++){
+		fix[i] = a[i-1];
+	}
+	return fix;
+}
+
+char* sabs(char* a){
+	if(a[0] == '-'){
+		char* fix = calloc(100, sizeof(char));
+		for(int i = 1; i < strlen(a); i++){
+			fix[i-1] = a[i];
+		}
+		return fix;
 	}else{
-		return b;
-	}
-}
-
-char* smin(char* a, char* b){
-	if(scmp(a, b) == -1){
 		return a;
-	}else{
-		return b;
 	}
 }
 
-void printint(int len, int* array){
-	for(int i = 0; i < len; i++){
+void int_print(int n, int* array){
+	for(int i = 0; i < n; i++){
 		printf("%d", array[i]);
 	}
 	printf("\n");
 }
 
+char* transform(int n, int* array){
+	int i = 0;
+	char* result = calloc(100, sizeof(char));
+	for(i = 0; i < n; i++){
+		if(array[i] == 0) continue;
+		if(array[i] != 0) break;
+	}
+	for(int j = 0; j < n-i; j++){
+		result[j] = '0' + array[i+j];
+	}
+	return result;
+}
+
 char* minus(char* a, char* b){
-	int mxlen = max(strlen(a), strlen(b))+1;
-	int* fa = calloc(200, sizeof(int));
-	int* fb = calloc(200, sizeof(int));
-	int* fc = calloc(200, sizeof(int));
-	int i;
-	for(i = 0; i < mxlen - strlen(a); i++){
-		fa[i] = 0;
+	int mx = max(strlen(a), strlen(b)); 
+	int* fia = calloc(100, sizeof(int));
+	int* fib = calloc(100, sizeof(int));
+	int* result = calloc(100, sizeof(int));
+	int i = 0;
+	for(i = 0; i < mx+1-strlen(a); i++){
+		fia[i] = 0;
 	}
-	for(int j = 0; j < strlen(a); j++){
-		fa[i+j] = a[j]-'0';
+	for(int j = 0; j < mx+1; j++){
+		fia[i+j] = a[j]-'0';
 	}
-	for(i = 0; i < mxlen - strlen(b); i++){
-		fb[i] = 0;
+	for(i = 0; i < mx+1-strlen(b); i++){
+		fib[i] = 0;
 	}
-	for(int j = 0; j < strlen(b); j++){
-		fb[i+j] = b[j]-'0';
+	for(int j = 0; j < mx+1; j++){
+		fib[i+j] = b[j]-'0';
 	}
-	for(int j = mxlen-1; j >= 1; j--){
-		if(fa[j] < fb[j]){
-			fc[j] = 10 - fb[j] + fa[j];
-			fa[j-1] -= 1;
+	//int_print(mx+1, fia);
+	//int_print(mx+1, fib);
+	result[0] = 0;
+	for(int j = mx; j >= 1; j--){
+		if(fia[j] < fib[j]){
+			result[j] = 10 - fib[j] + fia[j];
+			fia[j-1] -= 1;
 		}else{
-			fc[j] = fa[j] - fb[j];
+			result[j] = fia[j] - fib[j];
 		}
 	}
-	char* ans = calloc(200, sizeof(char));
-	int index = 0;
-	while(fc[index] == 0){
-		index += 1;
-	}
-	for(int j = 0; j < mxlen - index; j++){
-		ans[j] = fc[index+j] + '0';
-	}
-	return ans;
+	//int_print(mx+1, result);
+	return transform(mx+1, result);
 }
 
 char* plus(char* a, char* b){
-	int mxlen = max(strlen(a), strlen(b))+1;
-	int* fa = calloc(200, sizeof(int));
-	int* fb = calloc(200, sizeof(int));
-	int* fc = calloc(200, sizeof(int));
-	int i;
-	for(i = 0; i < mxlen - strlen(a); i++){
-		fa[i] = 0;
+	int mx = max(strlen(a), strlen(b)); 
+	int* fia = calloc(100, sizeof(int));
+	int* fib = calloc(100, sizeof(int));
+	int* result = calloc(100, sizeof(int));
+	int i = 0;
+	for(i = 0; i < mx+1-strlen(a); i++){
+		fia[i] = 0;
 	}
-	for(int j = 0; j < strlen(a); j++){
-		fa[i+j] = a[j]-'0';
+	for(int j = 0; j < mx+1; j++){
+		fia[i+j] = a[j]-'0';
 	}
-	for(i = 0; i < mxlen - strlen(b); i++){
-		fb[i] = 0;
+	for(i = 0; i < mx+1-strlen(b); i++){
+		fib[i] = 0;
 	}
-	for(int j = 0; j < strlen(b); j++){
-		fb[i+j] = b[j]-'0';
+	for(int j = 0; j < mx+1; j++){
+		fib[i+j] = b[j]-'0';
 	}
-	for(int j = mxlen-1; j >= 1; j--){
-		fc[j-1] += (fa[j]+fb[j]+fc[j])/10;
-		fc[j] += (fa[j]+fb[j])%10;
-		fc[j] %= 10;
+	//int_print(mx+1, fia);
+	//int_print(mx+1, fib);
+	result[0] = 0;
+	for(int j = mx; j >= 1; j--){
+		result[j-1] += (fia[j]+fib[j]+result[j])/10;
+		result[j] = (fia[j]+fib[j]+result[j])%10;
 	}
-	char* ans = calloc(200, sizeof(char));
-	int index = 0;
-	while(fc[index] == 0){
-		index += 1;
-	}
-	for(int j = 0; j < mxlen - index; j++){
-		ans[j] = fc[index+j] + '0';
-	}
-	return ans;
+	//int_print(mx+1, result);
+	return transform(mx+1, result);
 }
 
 char* mul(char* a, char* b){
-	char temp1[] = "0";
-	char temp2[] = "0";
-	char* ans = plus(temp1, temp2);
-	int offset = 0;
-	for(int i = strlen(b)-1; i >= 0; i--){
-		char* temp = plus(temp1, temp2);
-		for(int j = 0; j < b[i]-'0'; j++){
-			strcpy(temp, plus(temp, a));
+	char* result = calloc(100, sizeof(char));
+	result[0] = '0';
+	int index = 0;
+	for(int i = strlen(a)-1; i >= 0; i--){
+		char* temp = calloc(100, sizeof(char));
+		for(int j = 0; j < a[i]-'0'; j++){
+			strcpy(temp, plus(temp, b));
 		}
-		for(int j = 0; j < offset; j++){
+		for(int j = 0; j < index; j++){
 			temp[strlen(temp)] = '0';
 		}
-		offset += 1;
-		strcpy(ans, plus(ans, temp)); 
+		index += 1;
+		strcpy(result, plus(result, temp));
 	}
-	return ans;
+	return result;
 }
 
-int _plus(char* a, char* b){
+char* _plus(char* a, char* b){
+	char* fa = calloc(100, sizeof(char));
+	char* fb = calloc(100, sizeof(char));
+	char* zero = calloc(100, sizeof(char));
+	zero[0] = '0';
+	strcpy(fa, sabs(a));
+	strcpy(fb, sabs(b));
+	//printf("%s\n", sabs(a));
 	if(a[0] == '-' && b[0] == '-'){
-		printf("-%s\n", plus(sabs(a), sabs(b)));
+		return addn(plus(fa, fb));
 	}else if(a[0] == '-'){
-		if(scmp(sabs(a), sabs(b)) == 1){
-			printf("-%s\n", minus(sabs(a), sabs(b)));
-		}else if(scmp(sabs(a), sabs(b)) == -1){
-			printf("%s\n", minus(sabs(b), sabs(a)));
+		if(cmp(fa, fb) == -1){
+			return addn(minus(fa, fb));
+		}else if(cmp(fa, fb) == 0){
+			return zero;
 		}else{
-			printf("0\n");
+			return minus(fb, fa);
 		}
 	}else if(b[0] == '-'){
-		if(scmp(sabs(a), sabs(b)) == 1){
-			printf("%s\n", minus(sabs(a), sabs(b)));
-		}else if(scmp(sabs(a), sabs(b)) == -1){
-			printf("-%s\n", minus(sabs(b), sabs(a)));
+		if(cmp(fa, fb) == 1){
+			return addn(minus(fb, fa));
+		}else if(cmp(fa, fb) == 0){
+			return zero;
 		}else{
-			printf("0\n");
+			return minus(fa, fb);
 		}
 	}else{
-		printf("%s\n", plus(a, b));
+		return plus(fa, fb);
 	}
 }
 
-int _minus(char* a, char* b){
+char* _minus(char* a, char* b){
+	char* fa = calloc(100, sizeof(char));
+	char* fb = calloc(100, sizeof(char));
+	char* zero = calloc(100, sizeof(char));
+	zero[0] = '0';
+	strcpy(fa, sabs(a));
+	strcpy(fb, sabs(b));
+	//printf("%s\n", sabs(a));
 	if(a[0] == '-' && b[0] == '-'){
-		if(scmp(sabs(a), sabs(b)) == 1){
-			printf("-%s\n", minus(sabs(a), sabs(b)));
-		}else if(scmp(sabs(a), sabs(b)) == -1){
-			printf("%s\n", minus(sabs(b), sabs(a)));
+		if(cmp(fa, fb) == -1){
+			return addn(minus(fa, fb));
+		}else if(cmp(fa, fb) == 0){
+			return zero;
 		}else{
-			printf("0\n");
+			return minus(fb, fa);
 		}
 	}else if(a[0] == '-'){
-		if(scmp(sabs(a), sabs(b)) == 1){
-			printf("-%s\n", plus(sabs(a), sabs(b)));
-		}else if(scmp(sabs(a), sabs(b)) == -1){
-			printf("-%s\n", plus(sabs(b), sabs(a)));
-		}else{
-			printf("0\n");
-		}
+		return addn(plus(fa, fb)); 
 	}else if(b[0] == '-'){
-		printf("%s\n", plus(sabs(a), sabs(b)));
+		return plus(fa, fb);
 	}else{
-		if(scmp(sabs(a), sabs(b)) == 1){
-			printf("%s\n", minus(sabs(a), sabs(b)));
-		}else if(scmp(sabs(a), sabs(b)) == -1){
-			printf("-%s\n", minus(sabs(b), sabs(a)));
+		if(cmp(fa, fb) == -1){
+			return minus(fa, fb);
+		}else if(cmp(fa, fb) == 0){
+			return zero;
 		}else{
-			printf("0\n");
+			return addn(minus(fb, fa));
 		}
 	}
 }
 
-int _mul(char* a, char* b){
+char* _mul(char* a, char* b){
+	char* fa = calloc(100, sizeof(char));
+	char* fb = calloc(100, sizeof(char));
+	char* zero = calloc(100, sizeof(char));
+	zero[0] = '0';
+	strcpy(fa, sabs(a));
+	strcpy(fb, sabs(b));
 	if(a[0] == '-' ^ b[0] == '-'){
-		printf("-%s", mul(sabs(a), sabs(b)));
+		return addn(mul(fa, fb));
 	}else{
-		printf("%s", mul(sabs(a), sabs(b)));
+		return mul(fa, fb);
 	}
-} 
-
+}
 
 int main(){
 	char* a = calloc(100, sizeof(char));
 	char* b = calloc(100, sizeof(char));
 	scanf("%s", a);
 	scanf("%s", b);
-	_plus(a, b);
-	_minus(a, b);
-	_mul(a, b); 
-} 
+	printf("%s\n", _plus(a, b));
+	printf("%s\n", _minus(a, b));
+	printf("%s\n", _mul(a, b));
+}
+
